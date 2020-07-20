5ca37320d5848768403e5b51d9d58f52f0b8dc01
Mehrdad Kiani <mehrdad.kiani.eng@gmail.com>
2020-07-21 01:39:37 +0430

    docs: new link added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,6 +13,9 @@ class Header extends Component {
           <Nav.Link as={Link} to="/products">
             Products
           </Nav.Link>
+          <Nav.Link as={Link} to="/products">
+            Products
+          </Nav.Link>
         </Nav>
       </Navbar>
     );
